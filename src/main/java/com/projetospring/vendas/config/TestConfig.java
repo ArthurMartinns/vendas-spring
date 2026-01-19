2aac4eeb82e383b0f2db385752998c6634a9a704
ArthurMartinns <arthurmartiinscontato@gmail.com>
2026-01-19 11:34:42 -0300

    Payment entity

--- src/main/java/com/projetospring/vendas/config/TestConfig.java
+++ src/main/java/com/projetospring/vendas/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.projetospring.vendas.entities.Category;
 import com.projetospring.vendas.entities.Order;
 import com.projetospring.vendas.entities.OrderItem;
+import com.projetospring.vendas.entities.Payment;
 import com.projetospring.vendas.entities.Product;
 import com.projetospring.vendas.entities.User;
 import com.projetospring.vendas.entities.enums.OrderStatus;
@@ -78,5 +79,9 @@ public class TestConfig implements CommandLineRunner{
 
 		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 }
